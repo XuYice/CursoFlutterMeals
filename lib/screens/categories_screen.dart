c879b6210e1268712d1ceca8b9bc474c2d6e3138
Enzo Xu <yicexu@gmail.com>
2024-05-28 15:20:31 -0300

    first Commit

---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScrenn extends StatelessWidget {
+  const CategoriesScrenn({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
+      ),
+      children: <Widget>[],
+    );
+  }
+}
