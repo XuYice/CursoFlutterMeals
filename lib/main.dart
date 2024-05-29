aa2b2c4ccb89e4b3b548a775b658df18fa4026f5
Enzo Xu <yicexu@gmail.com>
2024-05-29 17:03:55 -0300

    Routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/categories_screen.dart';
+import 'screens/categories_meals_screen.dart';
+import 'utils/app.routes.dart';
 
 void main() {
   runApp(const MyApp());
@@ -26,7 +28,11 @@ class MyApp extends StatelessWidget {
               ),
             ),
       ),
-      home: const CategoriesScreen(),
+      initialRoute: AppRoutes.HOME,
+      routes: {
+        AppRoutes.HOME : (ctx) => const CategoriesScreen(),
+        AppRoutes.CATEGORIES_MEALS : (ctx) => const CategoriesMealsScreen(),
+      },
     );
   }
 }
--- lib/screens/categories_meals_screen.dart
+++ lib/screens/categories_meals_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import '../models/category.dart';
 
 class CategoriesMealsScreen extends StatelessWidget {
   const CategoriesMealsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final category = ModalRoute.of(context)!.settings.arguments as Category;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        title: Text(category.title),
+      ),
+      body: Center(
+        child: Text('Receitas por Categorias ${category.id}'),
+      ),
+    );
   }
 }
